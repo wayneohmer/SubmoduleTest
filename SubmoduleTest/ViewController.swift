6b8c2bc64b2797e947ee8ed90994085ccab43a6a
Wayne Ohmer <wayneohmer@gmail.com>
2018-02-15 17:13:06 -0800

    Initial Commit

---
+++ SubmoduleTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SubmoduleTest
+//
+//  Created by Wayne Ohmer on 2/15/18.
+//  Copyright © 2018 Wayne Ohmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
